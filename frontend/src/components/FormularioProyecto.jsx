30b18ca73038c1da90cdea22ccf52c1b7e0e0656
Gabriel Alfaro <gabriel.alfaro.cruz@gmail.com>
2023-10-24 16:37:15 -0600

    Creando proyecto

---
+++ frontend/src/components/FormularioProyecto.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react"
+
+import useProyectos from "../hooks/useProyectos"
+import Alerta from "./Alerta"
+
+const FormularioProyecto = () => {
+
+  const [nombre,setNombre] = useState("")
+  const [descripcion,setDescripcion] = useState("")
+  const [fechaEntrega,setFechaEntrega] = useState("")
+  const [cliente,setCliente] = useState("")
+
+  const {mostrarAlerta,alerta,submitProyecto} = useProyectos();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ([nombre,descripcion,fechaEntrega,cliente].includes('')){
+        mostrarAlerta({
+            msg: "Todo los campos son obligatorios",
+            error: true
+        })
+    return
+    }
+    //Pasar los datos hacia el provider.
+    await submitProyecto({nombre,descripcion,fechaEntrega,cliente})
+
+    setNombre('')
+    setDescripcion('')
+    setFechaEntrega('')
+    setCliente('')
+  }
+  const {msg} = alerta;
+
+
+  return (
+    <form 
+        className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow"
+        onSubmit={handleSubmit}
+    >
+            {msg && <Alerta alerta={alerta}/>}
+
+            {/* Nombre proyecto */}
+            <div className="mb-5" >
+                <label
+                    className="text-gray-700 uppercase font-bold text-sm"
+                    htmlFor="nombre"
+                >Nombre Proyecto</label>
+                <input 
+                    id="nombre"
+                    type="text"
+                    className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    placeholder="Nombre del proyecto"
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
+                />
+                
+            </div>
+             {/* Descripcion proyecto */}
+            <div className="mb-5" >
+                <label
+                    className="text-gray-700 uppercase font-bold text-sm"
+                    htmlFor="descripcion"
+                >Nombre Proyecto</label>
+                <textarea 
+                    id="descripcion"
+                    className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    placeholder="Descripcion del proyecto"
+                    value={descripcion}
+                    onChange={e => setDescripcion(e.target.value)}
+                />
+            </div>
+            {/* Fecha Entrega */}
+            <div className="mb-5" >
+                <label
+                    className="text-gray-700 uppercase font-bold text-sm"
+                    htmlFor="fecha-entrega"
+                >Fecha de Entrega</label>
+                <input 
+                    id="fecha-entrega"
+                    type="date"
+                    className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={fechaEntrega}
+                    onChange={e => setFechaEntrega(e.target.value)}
+                />
+            </div>
+            {/* Nombre del cliente */}
+            <div className="mb-5" >
+                <label
+                    className="text-gray-700 uppercase font-bold text-sm"
+                    htmlFor="cliente"
+                >Nombre Cliente</label>
+                <input 
+                    id="cliente"
+                    type="text"
+                    className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    placeholder="Nombre del Cliente"
+                    value={cliente}
+                    onChange={e => setCliente(e.target.value)}
+                />
+                
+            </div>
+
+            <input 
+                type="submit"
+                value="Crear Proyecto"
+                className="bg-sky-600 w-full p-3 uppercase font-bold text-white rounded cursor-pointer hover:bg-sky-700 transition-colors"
+            />
+    </form>
+  )
+}
+
+export default FormularioProyecto
